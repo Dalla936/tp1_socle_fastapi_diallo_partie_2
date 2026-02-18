aa84c5e754a940850a2ce98e1bedb9a64c5351a5
bouchaiblemaireatuniv <bouchaib.lemaire@univ-paris13.fr>
2026-02-18 13:11:07 +0100

    Mise a jour des ressources

--- partie_2/app/models_orm/user_table.py
+++ partie_2/app/models_orm/user_table.py
@@ -6,13 +6,11 @@ from sqlalchemy.orm import Mapped, mapped_column
 from app.db.base import Base
 
 
-class UserTable(Base):
+class UserTable:
     """
     Table ORM représentant l'entité User en base.
     """
 
-    __tablename__ = "users"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(Integer, autoincrement=True)
     login: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     age: Mapped[int] = mapped_column(Integer, nullable=False)
