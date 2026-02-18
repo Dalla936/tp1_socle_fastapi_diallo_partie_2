76fe80a84b5590960aa6c3bbc1b730f5d3d8e043
bouchaiblemaireatuniv <bouchaib.lemaire@univ-paris13.fr>
2026-02-18 13:12:41 +0100

    Mise a jours des ressources

--- partie_2/app/models_orm/user_table.py
+++ partie_2/app/models_orm/user_table.py
@@ -11,6 +11,6 @@ class UserTable:
     Table ORM représentant l'entité User en base.
     """
 
-    id: Mapped[int] = mapped_column(Integer, autoincrement=True)
-    login: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    age: Mapped[int] = mapped_column(Integer, nullable=False)
+    id: Mapped[int] = mapped_column(autoincrement=True)
+    login: Mapped[str] = mapped_column( unique=True, nullable=False)
+    age: Mapped[int] = mapped_column(nullable=False)
